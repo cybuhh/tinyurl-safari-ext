32fcf7204afd23024488af9fe102003cc89e8ba9
cybuhh <cybuhh@users.noreply.github.com>
2026-02-06 19:54:04 +0100

    small fetch url update

--- Shared (Extension)/Resources/popup/api.js
+++ Shared (Extension)/Resources/popup/api.js
@@ -1,24 +1,22 @@
-import { getApiToken } from './storage.js';
+import { getApiToken } from "./storage.js";
 
 export async function shortenUrl({ url, alias, description }) {
   const apiToken = await getApiToken();
+  const fetchUrl = `https://api.tinyurl.com/create?api_token=${apiToken}`;
   const payload = {
     url,
-    alias: alias.length >= 5 ? alias : '',
+    alias: alias.length >= 5 ? alias : "",
     description,
-    domain: "tinyurl.com"
+    domain: "tinyurl.com",
   };
 
-  const request = await fetch(
-    `https://api.tinyurl.com/create?api_token=${apiToken}`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(payload)
-    }
-  );
+  const request = await fetch(fetchUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  });
   const json = await request.json();
 
   return json.data.tiny_url;
